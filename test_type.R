3b619a6667d934890a9247586ae0a9321088dc46
jake32321 <jake32321@yahoo.com>
2015-11-23 16:51:09 -0600

    made the first commit

---
+++ test_type.R
@@ -0,0 +1,18 @@
+a <- 32 
+b <- "Jake"
+
+#Tells what the datatype of the different variables is.
+class(b)
+class(a)
+
+c <- 232
+d <- -7
+e <- 6.44
+f <- "hello world"
+
+#Checks to see if a value is numeric or not.
+is.numeric(c) #True
+is.numeric(d) #True
+is.numeric(e) #True
+is.numeric(f) #False
+
