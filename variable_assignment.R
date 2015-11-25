f3ebea5e5cb1905292192ea844958db605cadaeb
Jacob Reed <JacobReed@Jacobs-MacBook-Pro.local>
2015-11-24 22:39:47 -0600

    added new basic files

---
+++ variable_assignment.R
@@ -0,0 +1,20 @@
+# Assigns the value 42 to 'x'
+x <- 42
+
+# Prints out the value of the variable 'x'
+x
+
+# Assigns the value 5 to the variable called 'my_apples'
+my_apples <- 5
+
+# Prints out the value of the variable 'my_apples'
+my_apples
+
+# Assigns a value to the variables called my_oranges'
+my_oranges <- 6
+
+# Add these two variables together and print the result
+my_apples + my_oranges
+
+# Create the variable 'my_fruit'
+my_fruit <- my_apples + my_oranges
