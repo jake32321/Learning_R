3b3945397bd03bb3c1c73d817c74ff7b0e9e8ab3
Jacob Reed <JacobReed@Jacobs-MacBook-Pro.local>
2015-11-24 22:58:42 -0600

    added new basic files

---
+++ checking_types.R
@@ -0,0 +1,9 @@
+# Declares variables of different types
+my_numeric <- 42
+my_character <- "forty-two"
+my_logical <- FALSE
+
+# Checks which type these variables have:
+class(my_numeric)
+class(my_character)
+class(my_logical)
---
+++ vectors_with_r.R
@@ -0,0 +1,6 @@
+#Numeric vector
+numeric_vector <- c(1, 10, 49)
+#Char vector
+character_vector <- c("a", "b", "c")
+# Boolean vector
+boolean_vector <- c(TRUE, FALSE, TRUE)
